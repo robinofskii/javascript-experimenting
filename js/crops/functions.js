f48097870140d8955c4196306aa43d7c21124d77
Robin van den Berg <robinofski.development@gmail.com>
2022-10-25 15:42:38 +0200

    feat: :white_check_mark: add functions to solve environmental factor tests

--- js/crops/functions.js
+++ js/crops/functions.js
@@ -1,17 +1,37 @@
-const getYieldForPlant = (plant) => {
-  return plant.yield;
+const getDecimalFromPercentage = (percentage) => {
+  return 1 + percentage / 100;
 };
 
-const getYieldForCrop = (input) => {
+const getYieldForPlant = (plant, environmentFactors) => {
+  let sunFactor = 1;
+  let windFactor = 1;
+
+  if (environmentFactors?.sun) {
+    sunFactor = getDecimalFromPercentage(
+      plant.factor.sun[environmentFactors.sun]
+    );
+  }
+
+  if (environmentFactors?.wind) {
+    windFactor = getDecimalFromPercentage(
+      plant.factor.wind[environmentFactors.wind]
+    );
+  }
+
+  return plant.yield * sunFactor * windFactor;
+};
+
+const getYieldForCrop = (input, environmentFactors) => {
   const { crop, numCrops } = input;
-  return getYieldForPlant(crop) * numCrops;
+
+  return getYieldForPlant(crop, environmentFactors) * numCrops;
 };
 
-const getTotalYield = (input) => {
+const getTotalYield = (input, environmentFactors) => {
   let totalYield = 0;
 
   input.crops.forEach((crop) => {
-    totalYield += getYieldForCrop(crop);
+    totalYield += getYieldForCrop(crop, environmentFactors);
   });
 
   return totalYield;
